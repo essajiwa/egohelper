cab10ffc3774439906be7d3b0f0b0c7949b03c33
Essa Jiwa Permana <essa.jp@gmail.com>
2016-11-22 23:07:40 +0700

    Add GetDomain function helper

---
+++ net.go
@@ -0,0 +1,19 @@
+package egohelper
+
+import (
+	"bytes"
+	"strings"
+)
+
+// GetDomain : Extract only domain name with the TLD from a host string.
+// Subdomain and port (if any) will be cleaned up
+func GetDomain(host string) string {
+	if strings.Contains(host, ":") {
+		host = strings.Split(host, ":")[0]
+	}
+	domain := strings.SplitAfter(host, ".")
+	var buffer bytes.Buffer
+	buffer.WriteString(domain[len(domain)-2])
+	buffer.WriteString(domain[len(domain)-1])
+	return buffer.String()
+}
